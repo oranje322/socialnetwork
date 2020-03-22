85feae62e1b9931f0a61e60a5bb1dc72550efb44
oranje322 <shura.zyabrev@mail.ru>
2020-03-22 21:04:32 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import Profile from './components/Profile/Profile';
+import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
+import DialogsContainer from "./components/Dialogs/DialogsContainer";
+
+const App = (props) => {
+  return (
+      <div className='app-wrapper'>
+        <Header />
+        <Navbar />
+        <div className='app-wrapper-content'>
+          <Route path='/dialogs'
+                 render={ () => <DialogsContainer store={props.store} /> }/>
+
+          <Route path='/profile'
+                 render={ () => <Profile store={props.store} /> }/>
+        </div>
+      </div>
+  )
+}
+
+export default App;
