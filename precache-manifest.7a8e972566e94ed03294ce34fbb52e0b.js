4128bb4a3db7df62124be445343aa935bdae24e9
oranje322 <shura.zyabrev@mail.ru>
2020-05-01 02:11:21 +0300

    Updates

---
+++ precache-manifest.7a8e972566e94ed03294ce34fbb52e0b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8407c50a0cc1c45831990c161a0ca2f7",
+    "url": "/socialnetwork/index.html"
+  },
+  {
+    "revision": "0c77504ffe3a4fec76e4",
+    "url": "/socialnetwork/static/css/main.e5bd5428.chunk.css"
+  },
+  {
+    "revision": "9f3d35ee3b54736b9a84",
+    "url": "/socialnetwork/static/js/2.83ed5eff.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/socialnetwork/static/js/2.83ed5eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c77504ffe3a4fec76e4",
+    "url": "/socialnetwork/static/js/main.f43ca616.chunk.js"
+  },
+  {
+    "revision": "885225525c084534e01c",
+    "url": "/socialnetwork/static/js/runtime-main.d9576d12.js"
+  },
+  {
+    "revision": "022d2dd364866e76482819e5308b0dbb",
+    "url": "/socialnetwork/static/media/unnamed.022d2dd3.png"
+  },
+  {
+    "revision": "5f4fcfce4e6484276e1c462456ef505a",
+    "url": "/socialnetwork/static/media/user.5f4fcfce.png"
+  }
+]);
